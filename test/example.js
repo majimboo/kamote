a73f6171e92aafd6aa784b164edf58feb010cd9f
Majid Arif Siddiqui <me@majidarif.com>
2014-08-24 17:48:17 +0800

    added experimental basic protocol

--- test/example.js
+++ test/example.js
@@ -27,6 +27,54 @@ describe('example', function() {
         });
     });
 
+    it('should work with multiple parameters', function(done) {
+        // create a new server
+        var server = kamote.Server.create();
+        server.listen(7778);
+
+        // add a new method
+        server.add('remoteMethod', function(finish, care) {
+            if (finish && care) {
+                client.destroy();
+                server.close();
+                done();
+            }
+        });
+
+        // create a new client
+        var client = kamote.Client.create();
+        client.connect(7778);
+
+        // call remote function
+        client.on('ready', function() {
+            client.remoteMethod(true, true);
+        });
+    });
+
+    it('should work with object in parameters', function(done) {
+        // create a new server
+        var server = kamote.Server.create();
+        server.listen(7778);
+
+        // add a new method
+        server.add('remoteMethod', function(opts) {
+            if (opts.end) {
+                client.destroy();
+                server.close();
+                done();
+            }
+        });
+
+        // create a new client
+        var client = kamote.Client.create();
+        client.connect(7778);
+
+        // call remote function
+        client.on('ready', function() {
+            client.remoteMethod({ end: true });
+        });
+    });
+
     it('should work with errors in parameters', function(done) {
         // create a new server
         var server = kamote.Server.create();
