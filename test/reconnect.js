fcdb7341aca1d62a8da50416204ca7dce0ec9c88
Majid Arif Siddiqui <me@majidarif.com>
2014-08-27 19:48:14 +0800

    fixed disconnect test failing on travis 2

--- test/reconnect.js
+++ test/reconnect.js
@@ -3,23 +3,10 @@ var kamote = require('../');
 var net = require('net');
 
 describe('kamote-reconnect', function() {
-    it('should keep retrying until able to connect', function(done) {
-        // create a new client
-        var client = new kamote.Client();
-        client.reconnect(7345);
-
-        client.on('connect', function() {
-            done();
-        });
-
+    it('should emit disconnect when disconnected', function(done) {
         // create a new server
         var server = new kamote.Server();
-        // tried to delay this with setTimeout and it works
-        // but removed it just to make the test faster
-        server.listen(7345);
-    });
 
-    it('should emit disconnect when disconnected', function(done) {
         // create a new client
         var client = new kamote.Client();
         client.reconnect(5126);
@@ -29,13 +16,31 @@ describe('kamote-reconnect', function() {
         });
 
         client.on('connect', function() {
-            failserve.close();
+            server.close();
+        });
+
+        // tried to delay this with setTimeout and it works
+        // but removed it just to make the test faster
+        server.listen(5126);
+    });
+
+    it('should keep retrying until able to connect', function(done) {
+        // create a new client
+        var client = new kamote.Client();
+        client.reconnect(7345);
+
+        client.on('connect', function() {
+            done();
+        });
+
+        client.on('disconnect', function() {
+            console.log('retrying...');
         });
 
         // create a new server
-        var failserve = new kamote.Server();
+        var server = new kamote.Server();
         // tried to delay this with setTimeout and it works
         // but removed it just to make the test faster
-        failserve.listen(5126);
+        server.listen(7345);
     });
 });
